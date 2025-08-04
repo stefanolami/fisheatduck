f3081ac770d01874412b0f91d16b8053a4b1e672
stefanolami <stefanolami90@gmail.com>
2025-08-04 13:53:17 +0300

    feat: add favicons and home image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import Image from 'next/image'
+
 export default function Home() {
 	return (
-		<div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-			<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start text-4xl">
-				FISHEATDUCK
-			</main>
-		</div>
+		<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start text-4xl relative w-screen h-screen">
+			<Image
+				src="/FED_V2-bw.png"
+				alt="FISHEATDUCK"
+				fill
+				className="object-contain object-center"
+			/>
+		</main>
 	)
 }
