71b7ef3dd2b695f023e23b70a6a6644f9b7e8bfb
stefanolami <stefanolami90@gmail.com>
2025-08-04 16:38:13 +0300

    feat: add grainy background SVG to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,18 @@ import Image from 'next/image'
 
 export default function Home() {
 	return (
-		<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start text-4xl relative w-screen h-screen">
+		<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start text-4xl relative w-screen h-screen bg-[#C6C6C6]/50">
 			<Image
 				src="/FED_V2-bw.png"
 				alt="FISHEATDUCK"
 				fill
 				className="object-contain object-center"
+			/>
+			<Image
+				src="/grainy.svg"
+				alt="FISHEATDUCK"
+				fill
+				className="object-cover object-center absolute top-0 left-0 z-[-1]"
 			/>
 		</main>
 	)
