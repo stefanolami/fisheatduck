4a04503e1304b6c97f220e1fa87f2a7829280a87
stefanolami <stefanolami90@gmail.com>
2025-08-12 15:54:04 +0300

    feat: add noisy SVG image and wrap avatar image in a link for better accessibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,38 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
 	return (
-		<main className="relative w-screen h-screen bg-[#C6C6C6]/50">
-			<Image
-				src="/FED_V3_alpha_avatar.png"
-				alt="FISHEATDUCK"
-				fill
-				priority
-				sizes="50vw"
-				className="object-contain object-center"
-			/>
+		<main className="relative w-screen h-screen bg-[#C6C6C6]/50 text-[#fff2f2]">
+			<Link
+				href="https://en.wikipedia.org/wiki/Platypus"
+				target="_blank"
+			>
+				<Image
+					src="/FED_V3_alpha_avatar.png"
+					alt="FISHEATDUCK"
+					fill
+					priority
+					sizes="50vw"
+					className="object-contain object-center"
+				/>
+			</Link>
 			<span className="block text-3xl text-center absolute left-1/2 -translate-x-1/2 bottom-[18%] font-sankofa">
 				info@fisheatduck.com
 			</span>
-			<span className="block text-xs text-center absolute left-1/2 -translate-x-1/2 bottom-[14%] font-roboto">
-				I&apos;m confused
-			</span>
+			<Link
+				href="https://en.wikipedia.org/wiki/Platypus"
+				target="_blank"
+			>
+				<span className="block text-xs text-center absolute left-1/2 -translate-x-1/2 bottom-[14%] font-roboto">
+					I&apos;m confused
+				</span>
+			</Link>
 			<span className="block text-3xl text-center absolute left-1/2 -translate-x-1/2 bottom-[8%] font-roboto-mono">
 				info@fisheatduck.com
 			</span>
 			<Image
-				src="/grainy.svg"
+				src="/noisy.svg"
 				alt="FISHEATDUCK"
 				fill
 				className="object-cover object-center absolute top-0 left-0 z-[-1]"
